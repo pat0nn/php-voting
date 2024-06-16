8e284736995b15721cc481555e6cf93b734aca14
trong <phambatrong.pbt@gmail.com>
2024-06-16 22:20:41 +0700

    update

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,9 +1,8 @@
 <?php
-	$conn = new mysqli('192.168.31.229:3306', 'root', '1', 'votesystem');
+	$conn = new mysqli('localhost', 'root', '1', 'votesystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
 	
 ?>
-
